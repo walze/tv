8ad2ce9e3640e4a91393812eac4f2a6e7777e38a
walze <catufuzgu@gmail.com>
2019-09-21 12:26:22 -0300

    git push

---
+++ src/app.jsx
@@ -0,0 +1,72 @@
+import React, { useRef, useEffect, useState } from 'react'
+import { render } from 'react-dom'
+
+import channels from './channels.txt'
+
+const getChannels = () => fetch(channels)
+  .then(t => t.text())
+  .then(t => t.split(/\s/))
+
+const parent = document.querySelector('#player')
+
+const play = (source) => {
+  parent.innerHTML = ''
+
+  const clap = {
+    source,
+    height: '100vh',
+    width: '100%',
+    parent,
+    plugins: [LevelSelector, ChromecastPlugin],
+    mimeType: 'application/x-mpegURL',
+    autoPlay: true,
+    playInline: true,
+    mediacontrol: { seekbar: '#eee', buttons: '#eee' },
+    chromecast: {
+      appId: '9DFB77C0',
+      contentType: 'video/m3u8',
+      media: {
+        type: ChromecastPlugin.None,
+        title: 'Live',
+        subtitle: 'Live'
+      }
+    }
+  }
+
+  const player = new Clappr.Player(clap)
+  player.play()
+}
+
+
+// const $selector = document.querySelector('select')
+// $selector.addEventListener('change', ({ target }) => start(target.value))
+
+
+const App = () => {
+  const [channels, setChannels] = useState([])
+  const [currentChannel, setCurrent] = useState(null)
+
+  useEffect(() => {
+    getChannels().then(setChannels)
+
+    if (channels[currentChannel])
+      play(channels[currentChannel])
+  }, [currentChannel])
+
+  return (
+    <div>
+      <select defaultValue='null' onChange={({ target }) => setCurrent(Number(target.value))}>
+        <option disabled value='null'>Choose a Channel</option>
+        {channels.map((v, i) => (
+          <option key={v} value={i}>{i}</option>
+        ))}
+      </select>
+
+    </div>
+  )
+}
+
+render(
+  <App />,
+  document.querySelector('#root')
+)
